f27abe2a55a132c73990ed1a017438deddbae174
Chantreck <vladnetaev@mail.ru>
2022-03-25 00:34:55 +0700

    Added tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,43 @@
 fun main() {
-    println(encode(1888))
+    val encoder = Encoder()
+    val result = encoder.encode("abc")
+    println(result)
 }
 
-private val romanNumbers = mapOf(
-    1000 to "M", 900 to "CM", 500 to "D", 400 to "DM",
-    100 to "C", 90 to "XC", 50 to "L", 40 to "XL",
-    10 to "X", 9 to "IX", 5 to "V", 4 to "IV", 1 to "I"
-)
-private var counter = romanNumbers.mapValues { 0 }.toMutableMap()
+class Encoder {
+    private val romanNumbers = mapOf(
+        1000 to "M", 900 to "CM", 500 to "D", 400 to "DM",
+        100 to "C", 90 to "XC", 50 to "L", 40 to "XL",
+        10 to "X", 9 to "IX", 5 to "V", 4 to "IV", 1 to "I"
+    )
+    private var counter = romanNumbers.mapValues { 0 }.toMutableMap()
 
-private fun encode(num: Int): String {
-    var number = num
+    fun encode(num: String): String {
+        try {
+            num.toInt()
+        } catch (e: NumberFormatException) {
+            throw NumberFormatException("Provided not a number")
+        }
 
-    for (key in counter.keys) {
-        val div = number / key
-        counter[key] = div
-        number -= key * div
+        var number = num.toInt()
+
+        if (number < 1) {
+            throw IllegalArgumentException("Number must be greater than 0")
+        } else if (number >= 4000) {
+            throw IllegalArgumentException("Number must be less than 4000")
+        }
+
+        for (key in counter.keys) {
+            val div = number / key
+            counter[key] = div
+            number -= key * div
+        }
+
+        var result = ""
+        for (entry in counter.entries) {
+            result += romanNumbers[entry.key]?.repeat(entry.value)
+        }
+
+        return result
     }
-
-    var result = ""
-    for (entry in counter.entries) {
-        result += romanNumbers[entry.key]?.repeat(entry.value)
-    }
-
-    return result
 }
---
+++ src/test/kotlin/EncoderTest.kt
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class EncoderTest {
+    private val encoder = Encoder()
+
+    @Test
+    fun encoder_EncodeEasyNumber_GetCorrectString() {
+        val number = "327"
+        val result = encoder.encode(number)
+        assertEquals("CCCXXVII", result)
+    }
+
+    @Test
+    fun encoder_EncodeHardNumber_GetCorrectString() {
+        val number = "3888"
+        val result = encoder.encode(number)
+        assertEquals("MMMDCCCLXXXVIII", result)
+    }
+
+    @Test
+    fun encoder_EncodeString_ThrowException() {
+        val number = "abc"
+        val exception = assertThrows<NumberFormatException> {
+            encoder.encode(number)
+        }
+        assertEquals("Provided not a number", exception.message)
+    }
+
+    @Test
+    fun encoder_EncodeEmptyString_ThrowException() {
+        val number = ""
+        val exception = assertThrows<NumberFormatException> {
+            encoder.encode(number)
+        }
+        assertEquals("Provided not a number", exception.message)
+    }
+
+    @Test
+    fun encoder_EncodeTooLittleNumber_ThrowException() {
+        val number = "0"
+        val exception = assertThrows<IllegalArgumentException> {
+            encoder.encode(number)
+        }
+        assertEquals("Number must be greater than 0", exception.message)
+    }
+
+    @Test
+    fun encoder_EncodeTooBigNumber_ThrowException() {
+        val number = "4000"
+        val exception = assertThrows<IllegalArgumentException> {
+            encoder.encode(number)
+        }
+        assertEquals("Number must be less than 4000", exception.message)
+    }
+}
