a34af2be312fab5ec32f5b59bdf0c346ad6dbb49
Chantreck <vladnetaev@mail.ru>
2022-03-27 18:22:21 +0700

    Added bound tests

--- src/test/kotlin/EncoderTest.kt
+++ src/test/kotlin/EncoderTest.kt
@@ -8,36 +8,36 @@ class EncoderTest {
     private val encoder = Encoder()
 
     // Позитивные тесты
-    
+
     @TestFactory
-    fun encoder_EncodeNumber_GetCorrectString() = listOf(
-        DynamicTest.dynamicTest("encoder_EncodeEasyNumber_GetCorrectString") {
+    fun encoder_Number_GetCorrectString() = listOf(
+        DynamicTest.dynamicTest("encoder_EasyNumber_GetCorrectString") {
             assertEquals("CCCXXVII", encoder.encode("327"))
         },
-        DynamicTest.dynamicTest("encoder_EncodeFours_GetCorrectString") {
+        DynamicTest.dynamicTest("encoder_NumberOfFours_GetCorrectString") {
             assertEquals("CDXLIV", encoder.encode("444"))
         },
-        DynamicTest.dynamicTest("encoder_EncodeSixes_GetCorrectString") {
+        DynamicTest.dynamicTest("encoder_NumberOfSixes_GetCorrectString") {
             assertEquals("DCLXVI", encoder.encode("666"))
         },
-        DynamicTest.dynamicTest("encoder_EncodeNines_GetCorrectString") {
+        DynamicTest.dynamicTest("encoder_NumberOfNines_GetCorrectString") {
             assertEquals("CMXCIX", encoder.encode("999"))
         },
-        DynamicTest.dynamicTest("encoder_EncodeHardNumber_GetCorrectString") {
+        DynamicTest.dynamicTest("encoder_HardNumber_GetCorrectString") {
             assertEquals("MMMDCCCLXXXVIII", encoder.encode("3888"))
         },
-        DynamicTest.dynamicTest("encoder_EncodeLittleNumber_GetCorrectString") {
-            assertEquals("I", encoder.encode("1"))
+        DynamicTest.dynamicTest("encoder_GreaterThanLowerBoundNumber_GetCorrectString") {
+            assertEquals("C", encoder.encode("100"))
         },
-        DynamicTest.dynamicTest("encoder_EncodeBigNumber_GetCorrectString") {
-            assertEquals("MMMCMXCIX", encoder.encode("3999"))
+        DynamicTest.dynamicTest("encoder_LessThanHigherBoundNumber_GetCorrectString") {
+            assertEquals("MMMCMXC", encoder.encode("3990"))
         }
     )
 
     // Негативные тесты
 
     @Test
-    fun encoder_EncodeString_ThrowException() {
+    fun encoder_NotEmptyString_ThrowException() {
         val number = "abc"
         val exception = assertThrows<NumberFormatException> {
             encoder.encode(number)
@@ -46,7 +46,7 @@ class EncoderTest {
     }
 
     @Test
-    fun encoder_EncodeEmptyString_ThrowException() {
+    fun encoder_EmptyString_ThrowException() {
         val number = ""
         val exception = assertThrows<NumberFormatException> {
             encoder.encode(number)
@@ -55,7 +55,7 @@ class EncoderTest {
     }
 
     @Test
-    fun encoder_EncodeTooLittleNumber_ThrowException() {
+    fun encoder_LowerBoundNumber_ThrowException() {
         val number = "0"
         val exception = assertThrows<IllegalArgumentException> {
             encoder.encode(number)
@@ -64,8 +64,26 @@ class EncoderTest {
     }
 
     @Test
-    fun encoder_EncodeTooBigNumber_ThrowException() {
+    fun encoder_LessThanLowerBoundNumber_ThrowException() {
+        val number = "-100"
+        val exception = assertThrows<IllegalArgumentException> {
+            encoder.encode(number)
+        }
+        assertEquals("Number must be greater than 0", exception.message)
+    }
+
+    @Test
+    fun encoder_HigherBoundNumber_ThrowException() {
         val number = "4000"
+        val exception = assertThrows<IllegalArgumentException> {
+            encoder.encode(number)
+        }
+        assertEquals("Number must be less than 4000", exception.message)
+    }
+
+    @Test
+    fun encoder_GreaterThanHigherBoundNumber_ThrowException() {
+        val number = "10000"
         val exception = assertThrows<IllegalArgumentException> {
             encoder.encode(number)
         }
